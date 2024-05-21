e9b00f2abba1448176b75a911bea3907fcc18b44
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2024-05-21 20:25:06 +0800

    Update mytvsuper.py

--- mytvsuper.py
+++ mytvsuper.py
@@ -2,281 +2,140 @@ import requests
 import json
 
 CHANNEL_LIST = {
-'CWIN': {
-        'name': 'SUPER FREE',
-        'license': '0737b75ee8906c00bb7bb8f666da72a0:15f515458cdb5107452f943a111cbe89'
-    },
-'C18': {
-        'name': 'myTV SUPER 18台',
-        'license': '72de7d0a1850c8d40c5bdf9747a4ca7c:4967537ff0bc8209277160759de4adef'
-    },
-'TVG': {
-        'name': '黃金翡翠台',
-        'license': '8fe3db1a24969694ae3447f26473eb9f:5cce95833568b9e322f17c61387b306f'
-    },
-'JUHD': {
-        'name': '翡翠台(超高清)',
-        'license': '2c045f5adb26d391cc41cd01f00416fa:fc146771a9b096fc4cb57ffe769861be'
-    },
-'J': {
+    'J': {
         'name': '翡翠台',
-        'license': '0958b9c657622c465a6205eb2252b8ed:2d2fd7b1661b1e28de38268872b48480'
+        'license': '0958b9c657622c465a6205eb2252b8ed:2d2fd7b1661b1e28de38268872b48480',
+        'logo': 'https://github.com/wanglindl/TVlogo/blob/main/img/TVB1.png?raw=true',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=0958b9c657622c465a6205eb2252b8ed&key=2d2fd7b1661b1e28de38268872b48480'
     },
-'B': {
-        'name': 'J2',
-        'license': '56603b65fa1d7383b6ef0e73b9ae69fa:5d9d8e957d2e45d8189a56fe8665aaaa'
+    'JUHD': {
+        'name': '翡翠台 4K',
+        'license': '2c045f5adb26d391cc41cd01f00416fa:fc146771a9b096fc4cb57ffe769861be',
+        'logo': 'https://github.com/wanglindl/TVlogo/blob/main/img/TVB1.png?raw=true',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=2c045f5adb26d391cc41cd01f00416fa&key=fc146771a9b096fc4cb57ffe769861be'
     },
-'C': {
-        'name': '無綫新聞台',
-        'license': '90a0bd01d9f6cbb39839cd9b68fc26bc:51546d1f2af0547f0e961995b60a32a1'
+    'B': {
+        'name': 'TVBplus',
+        'license': '56603b65fa1d7383b6ef0e73b9ae69fa:5d9d8e957d2e45d8189a56fe8665aaaa',
+        'logo': 'https://raw.githubusercontent.com/wanglindl/TVlogo/main/img/TVB3.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=56603b65fa1d7383b6ef0e73b9ae69fa&key=5d9d8e957d2e45d8189a56fe8665aaaa'
     },
-'P': {
+    'P': {
         'name': '明珠台',
-        'license': 'e04facdd91354deee318c674993b74c1:8f97a629de680af93a652c3102b65898'
+        'license': 'e04facdd91354deee318c674993b74c1:8f97a629de680af93a652c3102b65898',
+        'logo': 'https://github.com/wanglindl/TVlogo/blob/main/img/TVB1.png?raw=true',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=e04facdd91354deee318c674993b74c1&key=8f97a629de680af93a652c3102b65898'
     },
-'A': {
-        'name': '無綫財經 體育 資訊台',
-        'license': '91db728a2806d0a0bbb9d92e608f5498:6e96ea66f51b2b9c16c5d6c5b3314a86'
+    'CWIN': {
+        'name': 'Super Free',
+        'license': '0737b75ee8906c00bb7bb8f666da72a0:15f515458cdb5107452f943a111cbe89',
+        'logo': 'https://raw.githubusercontent.com/sparkssssssssss/epg/main/logo/黄金翡翠台.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=0737b75ee8906c00bb7bb8f666da72a0&key=15f515458cdb5107452f943a111cbe89'
     },
-'CTVC': {
-        'name': '千禧經典台',
-        'license': '6c308490b3198b62a988917253653692:660578b8966fe8012ad51b9aae7a5d78'
+    'TVG': {
+        'name': '黄金翡翠台',
+        'license': '8fe3db1a24969694ae3447f26473eb9f:5cce95833568b9e322f17c61387b306f',
+        'logo': 'https://github.com/sparkssssssssss/epg/blob/main/logo/%E9%BB%84%E9%87%91%E7%BF%A1%E7%BF%A0%E5%8F%B0.png?raw=true',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=8fe3db1a24969694ae3447f26473eb9f&key=5cce95833568b9e322f17c61387b306f'
     },
-'CTVS': {
-        'name': '亞洲劇台',
-        'license': 'df5c0e617dffc3e3c44cb733dccb33c0:7d00ec9cd4f54d5baf94c03edc8cfe25'
+    'C': {
+        'name': '无线新闻台',
+        'license': '90a0bd01d9f6cbb39839cd9b68fc26bc:51546d1f2af0547f0e961995b60a32a1',
+        'logo': 'https://raw.githubusercontent.com/wanglindl/TVlogo/main/img/TVB4.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=90a0bd01d9f6cbb39839cd9b68fc26bc&key=51546d1f2af0547f0e961995b60a32a1'
     },
-'CDR3': {
-        'name': '華語劇台',
-        'license': 'baae227b5fc06e2545868d4a1c9ced14:8cd460458b0bdecca5c12791b6409278'
+    'CTVE': {
+        'name': '娱乐新闻台',
+        'license': '6fa0e47750b5e2fb6adf9b9a0ac431a3:a256220e6c2beaa82f4ca5fba4ec1f95',
+        'logo': 'https://github.com/sparkssssssssss/epg/blob/main/logo/%E5%A8%B1%E4%B9%90%E6%96%B0%E9%97%BB%E5%8F%B0.png?raw=true',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=6fa0e47750b5e2fb6adf9b9a0ac431a3&key=a256220e6c2beaa82f4ca5fba4ec1f95'
     },
-'TVO': {
-        'name': '黃金華劇台',
-        'license': 'acd93a5f665efd4feadb26f5ed48fd96:c6ce58ef9cce30638e0c2e9fc45a6dbd'
+    'PCC': {
+        'name': '凤凰卫视中文台',
+        'license': '7bca0771ba9205edb5d467ce2fdf0162:eb19c7e3cea34dc90645e33f983b15ab',
+        'logo': 'https://raw.githubusercontent.com/wanglindl/TVlogo/main/img/Phoenix1.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=7bca0771ba9205edb5d467ce2fdf0162&key=eb19c7e3cea34dc90645e33f983b15ab'
     },
-'CTVE': {
-        'name': '娛樂新聞台',
-        'license': '6fa0e47750b5e2fb6adf9b9a0ac431a3:a256220e6c2beaa82f4ca5fba4ec1f95'
+    'PIN': {
+        'name': '凤凰卫视资讯台',
+        'license': '83f7d313adfc0a5b978b9efa0421ce25:ecdc8065a46287bfb58e9f765e4eec2b',
+        'logo': 'https://raw.githubusercontent.com/wanglindl/TVlogo/main/img/Phoenix2.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=83f7d313adfc0a5b978b9efa0421ce25&key=ecdc8065a46287bfb58e9f765e4eec2b'
     },
-'CCOC': {
-        'name': '戲曲台',
-        'license': 'c91c296ef6c46b3f2af1da257553bd17:d6e92d5e594f6f8e494a6e1c9df75298'
+    'PHK': {
+        'name': '凤凰卫视香港台',
+        'license': 'cde62e1056eb3615dab7a3efd83f5eb4:b8685fbecf772e64154630829cf330a3',
+        'logo': 'https://raw.githubusercontent.com/wanglindl/TVlogo/main/img/Phoenix3.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=cde62e1056eb3615dab7a3efd83f5eb4&key=b8685fbecf772e64154630829cf330a3'
     },
-'KID': {
-        'name': 'SUPER Kids Channel',
-        'license': '42527ca90ad525ba2eac9979c93d3bca:b730006ad1da48b412ceb1f9e36a833d'
-    },
-'ZOO': {
-        'name': 'ZooMoo',
-        'license': '9c302eb50bef5a9589d97cb90982b05e:2603e646caafe22bc4e8a17b5a2dd55b'
-    },
-'CNIKO': {
-        'name': 'Nickelodeon',
-        'license': '0e69430290ed7b00af4db78419dcad8b:e4769b57a66e8e9737d6d86f317600c0'
-    },
-'CNIJR': {
-        'name': 'Nick Jr',
-        'license': '9f1385d2a12a67b572b9d968eb850337:3086bcd49a909606a8686858c05c7e33'
-    },
-'CCLM': {
-        'name': '粵語片台',
-        'license': '5b90da7fd2f018bf85a757241075626f:75c0897b4cf5ce154ddae86eddb79cd3'
-    },
-'CMAM': {
-        'name': '美亞電影台',
-        'license': 'c5d6f2afbd6b276312b0471a653828e1:ecbbb4a3ffa2200ae69058e20e71e91b'
-    },
-'CTHR': {
-        'name': 'Thrill',
-        'license': 'b22355363ab2b09a6def54be0c89b9f2:4b196c2bf24b37e82a81031246de6efe'
-    },
-'CCCM': {
-        'name': '天映經典頻道',
-        'license': '627b6ca150887912bec47ae4a9b85269:2bf49b2105d20544a6db89c0577b9802'
-    },
-'CMC': {
-        'name': '中國電影頻道',
-        'license': 'cabb16d20e71b512f24e9ece0cb09396:2d43505980a22014ee1a476880982308'
-    },
-'CRTX': {
-        'name': 'ROCK Action',
-        'license': '358eacad1f06e8e375493dabee96d865:461a02b2eb1232c6c100b95bd0bf40f8'
-    },
-'CKIX': {
-        'name': 'KIX',
-        'license': '3b4a44c5ef3217c55a357ad976d328b2:f3355e5a30722e631031b851642c27f1'
-    },
-'LNH': {
-        'name': 'Love Nature HD',
-        'license': '03fb0f439f942f50d06bf23a511bf4f8:bae7115da07195263e50ae5fc8bbe4f3'
-    },
-'LN4': {
-        'name': 'Love Nature 4K',
-        'license': '037c644cb92137ac5c8d653e952e4c8f:b3b2fcbe576a63cf3bbb9425da3de4cf'
-    },
-'SMS': {
-        'name': 'Global Trekker',
-        'license': 'a8f381c2a3114cc6c55f50b6ff0c78f3:86922e5993788488e1eca857c00d4fab'
-    },
-'CRTE': {
-        'name': 'ROCK綜藝娛樂',
-        'license': '002d034731b6ac938ea7ba85bc3dc759:6694258c023d73492a10acb860bc6161'
-    },
-'CAXN': {
-        'name': 'AXN',
-        'license': '20bea0e14af0d3dcb63d4126e8b50172:07382de357a2b0cceabe82e0b37cb8de'
-    },
-'GEM': {
-        'name': 'GEM',
-        'license': '5ff464c783f3a30b3cacab585a3ed42f:2325cbea6cd3ebe9c09d733426bfde7c'
-    },
-'CANI': {
-        'name': 'Animax',
-        'license': 'b1a073dbd8272b0c99940db624ce8d74:9fec26ff4c6774a8bde881e5cb0fe82e'
-    },
-'CJTV': {
-        'name': 'tvN',
-        'license': 'adcab9e8e5644ff35f04e4035cc6ad3b:d8e879e108a96fde6537c1b679c369b5'
-    },
-'CTS1': {
-        'name': '無線衛星亞洲台',
-        'license': 'ad7b06658e8a36a06def6b3550bde35c:b672f89570a630abb1d2abb5030e6303'
-    },
-'CRE': {
-        'name': '創世電視',
-        'license': 'adef00c5ba927d01642b1e6f3cedc9fb:b45d912fec43b5bbd418ea7ea1fbcb60'
-    },
-'FBX': {
-        'name': 'FashionBox',
-        'license': '4df52671ef55d2a7ac03db75e9bba2f7:4a3c16e8098c5021f32c7d4f66122477'
-    },
-'CMEZ': {
-        'name': 'Mezzo Live HD',
-        'license': 'e46f2747a9cf6822a608786bbc21d400:d8778fcf92c949e949a6700828f5f67e'
-    },
-'DTV': {
-        'name': '東方衛視國際頻道',
-        'license': '9d6a139158dd1fcd807d1cfc8667e965:f643ba9204ebba7a5ffd3970cfbc794c'
-    },
-'PCC': {
-        'name': '鳳凰衛視中文台',
-        'license': '7bca0771ba9205edb5d467ce2fdf0162:eb19c7e3cea34dc90645e33f983b15ab'
-    },
-'PIN': {
-        'name': '鳳凰衛視資訊台',
-        'license': '83f7d313adfc0a5b978b9efa0421ce25:ecdc8065a46287bfb58e9f765e4eec2b'
-    },
-'PHK': {
-        'name': '鳳凰衛視香港台',
-        'license': 'cde62e1056eb3615dab7a3efd83f5eb4:b8685fbecf772e64154630829cf330a3'
-    },
-'CMN1': {
-        'name': '神州新聞台',
-        'license': '7ee6ed08925f4716c8d0943e7bdb3e5f:6f3c1e31b30ccac36d466f41489ceb27'
-    },
-'CTSN': {
-        'name': '無線衛星新聞台',
-        'license': '73aaeb9e84db423627018017059e0f9d:34148a56250459383f7ef7369073bf39'
-    },
-'CCNA': {
-        'name': '亞洲新聞台',
-        'license': 'ddc7bb2603628134334919a0d7327d1d:a5fcd8bb852371faedd13b684f5adede'
-    },
-'CJAZ': {
-        'name': '半島電視台英語頻道',
-        'license': '80c76105d3ae35dfe25f939d1fb83383:6d76e7ba039773bced47d78e6de4fcf0'
-    },
-'CF24': {
-        'name': 'France 24',
-        'license': '2d4f6b8755a918d2126a2ee78791cf0b:c392acc1a1a070d2bcdf518d99d88406'
-    },
-'CDW1': {
-        'name': 'DW',
-        'license': '2bb557c09dfc01a27ab81778913f2a10:d00ca6eb9a83ffde846324109fb445ba'
-    },
-'CNHK': {
-        'name': 'NHK World-Japan',
-        'license': '9c2ecde1c31185ab61ed4689b87ae332:54895a656e053a73b39882e7a56d642b'
-    },
-'CARI': {
-        'name': 'Arirang TV',
-        'license': 'f3ae14e72f585eaf14b18d8d9515d43f:ce0e375c3966263877078aadd815742e'
-    },
-'EVT1': {
+    'EVT1': {
         'name': 'myTV SUPER直播足球1台',
-        'license': 'e8ca7903e25450d85cb32b3057948522:d5db5c03608f5f6c8a382c6abcb829e4'
-    },
-'EVT2': {
-        'name': 'myTV SUPER直播足球2台',
-        'license': '024f2733fb9afad23490149c601ce47c:034db34d56263c79c7f41448f2a6cfc1'
-    },
-'EVT3': {
-        'name': 'myTV SUPER直播足球3台',
-        'license': '84f456002b780253dab5534e9713323c:65aeb769264f41037cec607813e91bae'
-    },
-'EVT4': {
-        'name': 'myTV SUPER直播足球4台',
-        'license': '848d6d82c14ffd12adf4a7b49afdc978:3221125831a2f980139c34b35def3b0d'
-    },
-'EVT5': {
-        'name': 'myTV SUPER直播足球5台',
-        'license': '54700d7a381b80ae395a312e03a9abeb:7c68d289628867bf691b42e90a50d349'
-    },
+        'license': 'e8ca7903e25450d85cb32b3057948522:d5db5c03608f5f6c8a382c6abcb829e4',
+        'logo': 'https://raw.githubusercontent.com/sparkssssssssss/epg/main/logo/黄金翡翠台.png',
+        'license_key': 'https://vercel-php-clearkey-hex-base64-json.vercel.app/api/results.php?keyid=e8ca7903e25450d85cb32b3057948522&key=d5db5c03608f5f6c8a382c6abcb829e4'
+    }
 }
 
-
-def get_mytvsuper(channel: str):
+def get_mytvsuper(channel):
     if channel not in CHANNEL_LIST:
-        print('频道代号错误')
-        return
+        return '频道代号错误'
 
-    api_token = ''  # 请自行获取
+    api_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJib3NzX2lkIjoiODUwNDY0NTgwIiwiZGV2aWNlX3Rva2VuIjoiWHp6anMzYTVKTms2TFpnZmJQeFZHR2QzIiwiZGV2aWNlX2lkIjoiTldGalptSmxNREF0TkRJNU5TMDBaakV3TFRrNFl6QXRNbUkwT0RCbFpXRTJaVEV3IiwiZGV2aWNlX3R5cGUiOiJ3ZWIiLCJkZXZpY2Vfb3MiOiJicm93c2VyIiwiZHJtX2lkIjoiTldGalptSmxNREF0TkRJNU5TMDBaakV3TFRrNFl6QXRNbUkwT0RCbFpXRTJaVEV3IiwiZXh0cmEiOnsicHJvZmlsZV9pZCI6MX0sImlhdCI6MTcwOTgwNTA3NywiZXhwIjoxNzA5ODA4Njc3fQ.XG-C6gWxLgbBRQ3ttKn68AKMQLOg6SxTpbmHxXl_qI2dbjd1eFFmwB9kD1yd2N_X8HxLPBwJukD4lygIKxbBGrQQDY_1vNd76TldllaeE2BC3fUtc-kAFOU4JwbUkfFYsWVm3v2JP-YGM2GGlhFqN_3170WDAi2Gq-R0tZckeFNWk7jOSShqkE0E7L3eqJ09sDG76R-PCbdpnCIxaY_NXtoYRfIoVQZA9QysExUyO6hQGUxLaTvJDtflX_ZM3OiqTMndHp1p0cDsINnpFokD6Yby5XS18RjQ-Dn1XJznj7-sRjlaGgyIIBoJjxsR2oD5S8teA5S6x7w3Dv6uTO3bWVV9J60E6jguGVqKnSYJ4Rx8A1TgyUTT_g57key6UFIiEhkHYqk7s3H01V-lHffNp5zDo9UrCdaO6maW_ZeA85ohR6P1PMh9EakQ5A-vok60s2oqyokKHfyrQvcodsI-MTC9mKegjJzgV2-HBgyylyj6B2ewvE4icDD25UdspWgbc33NrRpe_kgPxgVKF4cgKCD-S1AT3WrOaqKnPfPvhqmlciwlpZrUqZg09BqcazWPoyWAp2nqf93H6tlDqMrtAQgvft3Nd8-cM7jYx-WvzqRrCRpZ8vRSv11UdezKzR2Jm4H64KTWbs3GxB5vboZaeypdEzQW6PipPpftqRnNMQU'  # 请自行获取
     headers = {
         'Accept': 'application/json',
-        'Authorization': f'Bearer {api_token}',
+        'Authorization': 'Bearer ' + api_token,
         'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
         'Host': 'user-api.mytvsuper.com',
         'Origin': 'https://www.mytvsuper.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.2 Safari/605.1.15',
         'Referer': 'https://www.mytvsuper.com/',
-        'X-Forwarded-For': '210.6.4.148',  # 香港原生IP
+        'X-Forwarded-For': '210.6.4.148'  # 香港原生IP  210.6.4.148
     }
 
     params = {
         'platform': 'android_tv',
-        'network_code': channel,
+        'network_code': channel
     }
 
-    response = requests.get(url='https://user-api.mytvsuper.com/v1/channel/checkout',
-                            params=params, headers=headers)
+    url = 'https://user-api.mytvsuper.com/v1/channel/checkout'
+    response = requests.get(url, headers=headers, params=params)
+
     if response.status_code != 200:
-        print('请求失败')
+        return '请求失败'
 
     response_json = response.json()
     profiles = response_json.get('profiles', [])
+
     play_url = ''
-    for i in profiles:
-        if i['quality'] == 'high':
-            play_url = i['streaming_path']
+    for profile in profiles:
+        if profile.get('quality') == 'high':
+            play_url = profile.get('streaming_path', '')
             break
 
     if not play_url:
-        print('未找到播放地址')
+        return '未找到播放地址'
+
     play_url = play_url.split('&p=')[0]
-    print('-------')
-    print('URL:', play_url)
-    print('-------')
-    print('License:', CHANNEL_LIST[channel]['license'])
-    print('-------')
-    license_dict = {s[0]: s[1] for s in [i.split(':') for i in CHANNEL_LIST[channel]['license'].split(';')]}
-    print('License Json:', json.dumps(license_dict))
-    print('-------')
 
+    license_key = CHANNEL_LIST[channel]['license_key']
+    channel_name = CHANNEL_LIST[channel]['name']
+    channel_logo = CHANNEL_LIST[channel]['logo']
+    m3u_content = f"#EXTINF:-1 tvg-id=\"{channel}\" tvg-logo=\"{channel_logo}\",{channel_name}\n"
+    m3u_content += "#KODIPROP:inputstream.adaptive.manifest_type=mpd\n"
+    m3u_content += "#KODIPROP:inputstream.adaptive.license_type=clearkey\n"
+    m3u_content += f"#KODIPROP:inputstream.adaptive.license_key={license_key}\n"
+    m3u_content += f"{play_url}\n"
 
-if __name__ == '__main__':
-    print('---- mytvsuper ----')
-    for channel in CHANNEL_LIST:
-        print(f'[{channel}] {CHANNEL_LIST[channel]["name"]}')
-    
-    channel = input('\n请输入频道代号:')
-    get_mytvsuper(channel)
+    return m3u_content
+
+# 创建或打开文件用于写入
+with open('mytvsuper.m3u', 'w', encoding='utf-8') as m3u_file:
+    # 写入 M3U 文件的头部
+    m3u_file.write("#EXTM3U url-tvg=\"https://xmltv.bph.workers.dev\"\n")
+
+    # 遍历所有频道并写入每个频道的 M3U 内容
+    for channel_code in CHANNEL_LIST.keys():
+        m3u_content = get_mytvsuper(channel_code)
+        m3u_file.write(m3u_content)
+
+print("所有频道的 M3U 播放列表已生成并保存为 'mytvsuper.m3u'。")
