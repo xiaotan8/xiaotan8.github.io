be77cebd595c46c09eb67ebcf0ae88e7dbfb28ee
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-05-17 18:01:14 +0800

    Update mytvsuper.py

--- mytvsuper.py
+++ mytvsuper.py
@@ -496,7 +496,7 @@ def encode_keys(hex_keyi_key):
 # 创建或打开文件用于写入
 with open('mytvsuper.m3u', 'w', encoding='utf-8') as m3u_file:
     # 写入 M3U 文件的头部
-    m3u_file.write("#EXTM3U url-tvg=\"https://mytvsuperepg.860775.xyz/epg.xml catchup-time="10800" catchup-type="timeshift"\"\n")
+    m3u_file.write('#EXTM3U url-tvg="https://mytvsuperepg.860775.xyz/epg.xml" catchup-time="10800" catchup-type="timeshift"\n')
 
     # 遍历所有频道并写入每个频道的 M3U 内容
     for channel_code in CHANNEL_LIST.keys():
